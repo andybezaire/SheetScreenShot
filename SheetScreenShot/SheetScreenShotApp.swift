7d968baf660dae0c0bc1bc52a1b1f61e81667567
Andy Bezaire <andy.bezaire@assaabloy.com>
2022-08-23 12:57:03 +0300

    Initial Commit

---
+++ SheetScreenShot/SheetScreenShotApp.swift
@@ -0,0 +1,17 @@
+//
+//  SheetScreenShotApp.swift
+//  SheetScreenShot
+//
+//  Created by Andy Bezaire on 23.8.2022.
+//
+
+import SwiftUI
+
+@main
+struct SheetScreenShotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
